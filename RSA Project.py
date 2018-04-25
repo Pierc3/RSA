53fbbaf5d6e7e782fd67d0e072172b94adc81552
PrestonCorley <31449484+PrestonCorley@users.noreply.github.com>
2018-04-25 11:16:54 -0700

    Add files via upload

--- RSA Project.py
+++ RSA Project.py
@@ -2,10 +2,13 @@
 
 
 ##option to encrypt a message goes here
-e = raw_input("Please enter the E value (smallest co-prime number between your P and Q values):")
-n = raw_input("Please enter the N value (P * Q):")
-message = raw_input("Please enter the message you wish to encrypt:")
+e = input("Please enter the e value (smallest co-prime number between your P and Q values):")
+n = input("Please enter the n value (P * Q):")
+
+message = input("Please enter the message you wish to encrypt:")
 encrypted_message = ''
+encrypted_message2 = ''
+
 
 LUT_encryption = dict()
 LUT_decryption = dict()
@@ -13,19 +16,32 @@ LUT_decryption = dict()
 for x in message:
     if x in LUT_encryption:
         encrypted_message += LUT_encryption[x]
+
     else:
         numerize = ord(x)
         encrypt = pow(numerize, e, n)
-        denumerize = unichr(encrypt)
+        denumerize = chr(encrypt)
         encrypted_message += denumerize
         LUT_encryption[x] = denumerize
 
 print encrypted_message
 
 
-
-
 ##option to decrypt a message goes here
+
+for t in encrypted_message:
+  if x in LUT_encryption:
+    encrypted_message == LUT_encryption[x]
+  else: 
+        numerize and [x]
+        decrypt = pow(numerize, e, n)
+        encrypted_message += denumerize
+        LUT_encryption[x]
+
+print encrypted_message2
+
+
+
 ##option to exit goes here
 #encryption function goes here
 #decryption function goes here
