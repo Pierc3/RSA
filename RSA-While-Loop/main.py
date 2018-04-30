e2cf1d88133658aceab7e1d8b3eb035b47543fb3
PrestonCorley <31449484+PrestonCorley@users.noreply.github.com>
2018-04-30 10:34:35 -0700

    Add files via upload

---
+++ RSA-While-Loop/main.py
@@ -0,0 +1,9 @@
+#while loop that controls the entire program goes here
+encryption_program = True 
+while encryption_program: 
+
+  ##option to exit goes here
+  stop = input("Press S to stop, press C to continue")
+  if stop == ('S'):
+    encryption_program = False
+  
