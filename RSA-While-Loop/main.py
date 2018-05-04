e34a16f37f029a798ee1354eed199e43798eeb48
Pierce Gregor <31449131+Pierc3@users.noreply.github.com>
2018-05-04 10:18:28 -0700

    Delete main.py

--- RSA-While-Loop/main.py
+++
@@ -1,9 +0,0 @@
-#while loop that controls the entire program goes here
-encryption_program = True 
-while encryption_program: 
-
-  ##option to exit goes here
-  stop = input("Press S to stop, press C to continue")
-  if stop == ('S'):
-    encryption_program = False
-  
